cc44dad6ec067acdefc1dd0a8769a7d49739a701
Mike Zhang <mikexd.dev@gmail.com>
2024-02-01 11:42:25 +0800

    update deploy script to deploy all contracts in folder

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,14 +1,16 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { Contract } from "ethers";
+import fs from "fs";
+import path from "path";
 
 /**
- * Deploys a contract named "YourContract" using the deployer account and
+ * Deploys all contracts in the contracts folder using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployAllContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -22,23 +24,32 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
-    from: deployer,
-    // Contract constructor arguments
-    args: [deployer],
-    log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
-  });
+  // Get all contract files in the contracts directory
+  const contractsDir = path.join(__dirname, "..", "contracts");
+  const contractFiles = fs.readdirSync(contractsDir);
 
-  // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  // Loop through each contract file and deploy
+  for (const contractFile of contractFiles) {
+    const contractName = path.basename(contractFile, path.extname(contractFile));
+
+    await deploy(contractName, {
+      from: deployer,
+      // Contract constructor arguments
+      args: [deployer],
+      log: true,
+      // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+      // automatically mining the contract deployment transaction. There is no effect on live networks.
+      autoMine: true,
+    });
+
+    // Get the deployed contract to interact with it after deploying.
+    const contract = await hre.ethers.getContract<Contract>(contractName, deployer);
+    console.log(`👋 Contract ${contractName} deployed, address: ${await contract.getAddress()}`);
+
+    // Tags are useful if you have multiple deploy files and only want to run one of them.
+    // e.g. yarn deploy --tags YourContract
+    deployAllContracts.tags = [contractName];
+  }
 };
 
-export default deployYourContract;
-
-// Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+export default deployAllContracts;
